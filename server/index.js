98f30d903a1464fff7210ee720fef401d40ce704
Raihan-004 <82903109+Raihan-004@users.noreply.github.com>
2022-07-21 23:14:38 +0600

    5th week

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b264i.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get('/',(req,res)=>{
+    res.send("hello form db")
+})
+
+const client = new MongoClient(uri,{useNewUrlParser: true});
+client.connect(err=>{
+    const reviewCollection = client.db("CholoBD").collection("review");
+    app.post('/addReview',(req,res)=>{
+        const review = req.body;
+        reviewCollection.insertOne(review)
+        .then(result => {
+            res.send(result.insertedCount)
+        })
+    })
+});
+app.listen(process.env.PORT || port)
