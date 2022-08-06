548d4ee8ed9abc25430d82b338f091357900e104
Dipto <tdipto@yahoo.com>
2022-08-06 21:47:38 +0600

    dipto final

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 /* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b264i.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; */
 const uri =
-  "mongodb+srv://Dipto:dipto@cluster0.b264i.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Dipto:Dipto007@cluster0.oqm6q2i.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
 
 app.use(bodyParser.json());
